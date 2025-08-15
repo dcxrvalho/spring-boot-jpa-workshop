5b9211fc84687cfb54948e3f23a28645b95aa615
dcxrvalho <densetsunoban@gmail.com>
2025-08-15 14:14:26 -0300

    Project created

---
+++ src/main/java/io/github/dcxrvalho/spring_boot_jpa_workshop/SpringBootJpaWorkshopApplication.java
@@ -0,0 +1,13 @@
+package io.github.dcxrvalho.spring_boot_jpa_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dcxrvalho/spring_boot_jpa_workshop/SpringBootJpaWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dcxrvalho.spring_boot_jpa_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
